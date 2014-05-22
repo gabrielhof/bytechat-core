8a0d76ac1650fc12fb007c6ee6ec95e888723a61
Gabriel Hoff <gabriel@i360tecnologia.com.br>
2014-05-22 13:17:09 -0300

    properties para configuração

--- src/main/java/br/feevale/bytechat/config/Configuration.java
+++ src/main/java/br/feevale/bytechat/config/Configuration.java
@@ -1,5 +1,9 @@
 package br.feevale.bytechat.config;
 
+import java.util.Properties;
+
+import org.apache.commons.lang.StringUtils;
+
 public class Configuration {
 
 	private String host;
@@ -19,6 +23,32 @@ public class Configuration {
 
 	public void setPort(int port) {
 		this.port = port;
+	}
+	
+	public static Configuration fromProperties() {
+		try {
+			Properties properties = new Properties();
+			properties.load(Configuration.class.getClassLoader().getResourceAsStream("settings.properties"));
+			
+			Configuration config = new Configuration();
+			config.setHost(properties.getProperty("server.host", "localhost"));
+			config.setPort(Integer.parseInt(properties.getProperty("server.port", "8080")));
+			
+			String hostEnv = System.getenv("CHAT_SERVER_HOST");
+			String portEnv = System.getenv("CHAT_SERVER_PORT");
+			
+			if (StringUtils.isNotBlank(hostEnv)) {
+				config.setHost(hostEnv);
+			}
+			
+			if (StringUtils.isNotBlank(portEnv)) {
+				config.setPort(Integer.parseInt(portEnv));
+			}
+			
+			return config;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 	
 }
